9466a2d8d7d8cec2747ab88f650203e4b35b9d73
asawarijoshi2501 <asawari.joshi@aalto.fi>
2020-06-03 00:39:38 +0300

    testing 5

--- test.py
+++ test.py
@@ -7,7 +7,19 @@ opts.set_headless()
 assert opts.headless  # Operating in headless mode
 browser = Firefox(options=opts)
 browser.get('https://asawarijoshi2501.github.io/selenium-test/')
+
+print("Test case 1 : find if element by id 'about' exists")
 search_form = browser.find_element_by_id('about')
-assert(search_form)
+if assert(search_form):
+  print("First case passed")
+
+print("Test case 1 : find if element by id 'image' exists")
+search_form = browser.find_element_by_id('image')
+if assert(search_form):
+  print("second case passed")
+
+
+
+
 
 
