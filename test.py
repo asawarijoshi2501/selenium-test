28c26a8f3fd77e350478aa37a0f253df5184992e
asawarijoshi2501 <asawari.joshi@aalto.fi>
2020-06-03 00:33:36 +0300

    testing 4

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ opts = Options()
 opts.set_headless()
 assert opts.headless  # Operating in headless mode
 browser = Firefox(options=opts)
-browser.get('https://asawarijoshi2501.github.io/devops101/')
+browser.get('https://asawarijoshi2501.github.io/selenium-test/')
 search_form = browser.find_element_by_id('about')
 assert(search_form)
 
